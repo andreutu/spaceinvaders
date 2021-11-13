2b0b8b5b946b92d5c8142ede31fbcfd97d5d0da1
andreutu <roanflo05@gmail.com>
2021-11-13 09:38:02 +0200

    creeare proiect

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D corp;
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        corp = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Input.GetAxis("Horizontal");
+
+        var position = corp.transform.position;
+        var newPos = position + Vector3.right * input * moveSpeed * Time.deltaTime;
+
+        corp.MovePosition(newPos);
+    }
+}
